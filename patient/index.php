f83cee621f0c66d5d20c34bcbb5bb546174928f7
Fatima Nahid-Dev <fatimanahid341@gmail.com>
2025-07-16 23:01:46 +0500

    Update index.php

--- patient/index.php
+++ patient/index.php
@@ -38,7 +38,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
     <title>User Registration | Patient</title>
 
     <!-- CSS -->
